b47ef28a0b0f829380ab73b0a9ef7dec2d9cdc3b
Alina Rodina <alnardn207@gmail.com>
2022-12-27 21:55:03 +0300

    fifth task done

---
+++ fifth_task.py
@@ -0,0 +1,9 @@
+def sumSquaredFirstNNums(n):
+	return n * (n + 1) * (2 * n + 1) / 6
+
+def squareOfSumFirstNNums(n):
+	return n * (n + 1) / 2
+
+print(sumSquaredFirstNNums(100))
+print(squareOfSumFirstNNums(100))
+
